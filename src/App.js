b8e8d7111146b9c51305b80d71acabb9a3abed87
Miqueas Guerrero <129316828+Miqueas0@users.noreply.github.com>
2023-06-28 19:39:38 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { react,useState } from "react";
+import "./App.css";
+import { Button, Divider } from 'antd';
+
+function App() 
+  {      
+    const [valor, setValor]=useState(0);
+ 
+  
+const sumar = () =>{
+  setValor(valor+1);
+}
+const restar = () =>{
+  setValor(valor-1);
+}
+const restablecer = () =>{
+  setValor(0);
+}
+const [size, setSize] = useState('large');
+  return (
+    
+    <div>    
+    <p><span> { valor } </span></p>
+    <Divider style={{'border-color':'black'}}>Contador</Divider>
+    <br></br>
+    <table value={size} align="center">
+      <tr>
+    <Button style={{ background: "rgb(50, 130, 246)", borderColor: "black" }} size="large" type="primary" onClick={sumar}>Aumentar</Button>
+      </tr>
+      <tr>
+    <Button  style={{ background: "rgb(50, 130, 246)", borderColor: "black" }} size="large" type="primary" onClick={restar}>Disminuir</Button>
+      </tr>
+      <tr>
+    <Button  style={{ paddingRight: "33px",paddingLeft: "29px", background: "rgb(50, 130, 246)", borderColor: "white" }} size="large" type="primary" onClick={restablecer}>reset</Button>
+      </tr>
+    </table>
+    </div>
+
+  );
+  }
+
+export default App;
